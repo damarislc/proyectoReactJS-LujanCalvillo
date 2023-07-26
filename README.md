f006982fe0db17f932e9552b19b64e533667bcd0
damarislc <damarislc9@gmail.com>
2023-07-26 09:32:58 -0400

    Se añade variables de ambiente

---
+++ README.md
@@ -0,0 +1,29 @@
+# Librería DamaLu
+
+![](https://res.cloudinary.com/doxztm7ed/image/upload/v1687198242/225932_ugadgb.png)
+
+## Herramientas utilizadas
+
+- React JS
+- Vite
+- Firebase
+- MUI
+
+### Clonar el codigo
+
+```
+git clone https://github.com/damarislc/proyectoReactJS-LujanCalvillo.git
+```
+
+### Inicializar el código
+
+```
+npm install
+```
+
+[link](google.com)
+
+```javascript
+const variable = "mi valor";
+//de luuujooo
+```
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqHm8LJMP-OK6JjowJclzXbQ3JnPMDMVM",
-  authDomain: "libreria-damalu.firebaseapp.com",
-  projectId: "libreria-damalu",
-  storageBucket: "libreria-damalu.appspot.com",
-  messagingSenderId: "765706673916",
-  appId: "1:765706673916:web:2612de5760bfcf369e8179",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
